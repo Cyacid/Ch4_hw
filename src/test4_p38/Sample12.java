199ca3590bab616a997bb7fb2f3411a6c3b07f0c
wensen514 <wensen514@gmail.com>
2020-11-30 21:41:27 +0800

    First

---
+++ src/test4_p38/Sample12.java
@@ -0,0 +1,40 @@
+package test4_p38;
+
+public class Sample12 {
+	public static void main(String[] args) {
+		iVehicle[] v=new iVehicle[2];
+		v[0] = new Car(1234,20.5);
+		v[1] = new Plane(232);
+		for(int i=0;i<v.length;i++) {
+			v[i].show();
+		}
+	}
+}
+interface iVehicle{
+	int weight = 1000;
+	void show();
+}
+class Car implements iVehicle{
+	private int num;
+	private double gas;
+	public Car(int n,double g) {
+		num = n;
+		gas = g;
+		System.out.println("生產了車號為"+num+"，汽油量為"+gas+"的車子");
+	}
+	public void show() {
+		System.out.println("車號是"+num);
+		System.out.println("汽油量是"+gas);	
+	}
+}
+class Plane implements iVehicle{
+	private int flight;
+	
+	public Plane(int f) {
+		flight	 = f;
+		System.out.println("生產了"+flight+"班次的飛機");
+	}
+	public void show() {
+		System.out.println("飛機的班次是"+flight);
+	}
+}
